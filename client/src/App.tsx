483ac10586a88197a19fab4420ebb57a3ce29ae9
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-07-21 16:43:56 +0530

    Intial client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Welcome to the Quiz Generator</h1>
+    </div>
+  )
+}
+
+export default App
